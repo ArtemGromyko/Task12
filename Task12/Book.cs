7e607871ebe98aa56c0c873c90c17acb3ec41430
RezeR1320 <Artem@DESKTOP-J9AHB8E>
2020-11-02 03:25:48 +0300

    Добавьте файлы проекта.

---
+++ Task12/Book.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Text.Json;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
+
+namespace Task12
+{
+    [DataContract]
+    public class Book
+    {
+        [DataMember]
+        public string Name { get; set; }
+        [DataMember]
+        public int NumberOfPages { get; set; }
+        [DataMember]
+        public string Author { get; set; }
+        public Book() { }
+        public Book(string name, int number, string author)
+        {
+            Name = name;
+            NumberOfPages = number;
+            Author = author;
+        }
+        public void Disp()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("NumberOfPages: " + NumberOfPages);
+            Console.WriteLine("Author: " + Author);
+        }
+        public void SerializeXML(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                DataContractSerializer ds = new DataContractSerializer(typeof(Book));
+                ds.WriteObject(fs, this);
+            }
+        }
+        public static Book DeserializeXML(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                DataContractSerializer ds = new DataContractSerializer(typeof(Book));
+                return (Book)ds.ReadObject(fs);
+            }
+        }
+        public void SerializeJSON(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                DataContractJsonSerializer ds = new DataContractJsonSerializer(typeof(Book));
+                ds.WriteObject(fs, this);
+            }
+        }
+        public static Book DeserializeJSON(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            {
+                DataContractJsonSerializer ds = new DataContractJsonSerializer(typeof(Book));
+                return (Book)ds.ReadObject(fs);
+            }
+        }
+    }
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book b1 = new Book("Book1", 250, "Tom");
+            b1.SerializeXML(@"..\..\..\Serialize.xml");
+            Book b2 = Book.DeserializeXML(@"..\..\..\Serialize.xml");
+            b2.Disp();
+            b2.SerializeJSON(@"..\..\..\Serialize.json");
+            Console.WriteLine();
+            Book b3 = new Book();
+            b3 = Book.DeserializeXML(@"..\..\..\Serialize.xml");
+            b3.Disp();
+        }
+    }
+}
